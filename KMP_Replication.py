1ed8c05aecab13ae87e9801f6203af37184276de
lt1245 <laszlotetenyi@gmail.com>
2016-03-26 14:57:14 -0400

    Fixed : stationary distribution, lowest aprime

--- KMP_Replication.py
+++ KMP_Replication.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 from scipy.misc import derivative as deriv
 import scipy.linalg as slin
 from numba import jit
-
+from scipy.optimize import root
 
 'Parameters'
 rho_z = 0.79
@@ -23,15 +23,19 @@ eta = 0.32
 theta = 0.8
 beta = 0.88
 sigma = 2.0
-dampen_coeff = 0.3 # For both the Bellman iteration and Newton Iteration
-dampen_newton = 0.3 # For the aprime updating in newton method  
+dampen_coeff_start = 0.1 # For both the Bellman iteration and Newton Iteration
+# but only a strating value
+fast_coeff = 10 # Number of iterations after the Newton ieration with 1.0 takes over
+dampen_newton = 1.0 # For the aprime updating in newton method  
 
 'Asset grid and z grid'
-a_lower = 0.001
-a_upper = 50.0
-n_a = 10   #number of grid points for assets
+a_lower = 0.05
+a_upper = 30.0
+n_a = 10  #number of grid points for assets
+n_d = 20 #number of grid points for assets in the stationary distribution
 n_z = 5   #number of grid points for productivity
-n_s = n_a * n_z 
+n_s = n_a * n_z #Overall number of gridpoints
+n_ds = n_d * n_z #Overall number of gridpoints in the stationary distribution
 #lin_stop = np.log(np.log(np.log(a_upper - a_lower + 1.)+1)+1) 
 #agrid = np.exp(np.exp(np.exp(np.linspace(0, lin_stop, n_a)) - 1.) - 1.) -1. + a_lower
 #agrid = np.reshape(agrid,(n_a,1))
@@ -47,22 +51,24 @@ Polyname = ('spli','spli')
 Order = (0,0)
 Phi_s = ip.funbas(P,s,Order,Polyname)
 
-'Load initial guess for consumption and coefficients'
-c_guess=np.reshape(np.loadtxt("c_guess.csv"),(n_s,1))
-coeff_guess=np.reshape(np.loadtxt("coeff_guess.csv"),(n_s,1))
-coeff_e_guess=np.reshape(np.loadtxt("coeff_e_guess.csv"),(n_s,1))
-#c_vec = 1.0 * np.ones((n_s,1)) 
-#coeff= coeff_guess
-#coeff_e= coeff_e_guess
-#coeff =  slin.solve(Phi_s,V) 
-#coeff_e = slin.solve(Phi_s , np.kron(T , np.eye(n_a)) @ Phi_s @ coeff)
-
-
 'Starting guess for coefficients - start at the steady state value function'
 cons_start =  ( (1.0 + eta **(1.0-epsilon) ) ** (-nu/(1.0-epsilon)) + 0.02 * (s[:,0,None] + (10.0- a_lower)) ) /(1.0 + eta)
 house_start = eta * cons_start
 V = 1.0 / (1.0 - beta) /(1.0-sigma) * ((cons_start ** ((epsilon-1.0)/epsilon) + eta **(1.0/epsilon) * house_start** ((epsilon-1.0)/epsilon) ) \
 ** ((epsilon-1.0)/epsilon) - (( (1.0 + eta **(1.0-epsilon) ) ** (-nu/(1.0-epsilon))))** (1.0+1.0/nu) / (1.0+1.0/nu)) ** (1.0 -sigma)
+
+'Load initial guess for consumption and coefficients'
+#c_guess=np.reshape(np.loadtxt("c_guess.csv"),(n_s,1))
+#coeff_guess=np.reshape(np.loadtxt("coeff_guess.csv"),(n_s,1))
+#coeff_e_guess=np.reshape(np.loadtxt("coeff_e_guess.csv"),(n_s,1))
+#c_vec = 1.0 * np.ones((n_s,1)) 
+#coeff= coeff_guess
+#coeff_e= coeff_e_guess
+#coeff =  slin.solve(Phi_s,V) 
+#coeff_e = slin.solve(Phi_s , np.kron(T , np.eye(n_a)) @ Phi_s @ coeff)
+c_guess = 1.0 * np.ones((n_s,1))
+coeff_guess = slin.solve(Phi_s,V)
+coeff_e_guess = slin.solve(Phi_s , np.kron(T , np.eye(n_a)) @ Phi_s @ coeff_guess)
 
 'GHH preferences (Penalizing values for which g > disutility from working)'
 @jit
@@ -133,7 +139,6 @@ def aprimefunc(s,c):
     a = np.reshape(s[:,0],(n_S,1))
     z = np.reshape(s[:,1],(n_S,1))
     c = np.reshape(c,(n_S,1))
-    
     def G(c,a,z):
         m_util_ch = eta * (u * q_t) ** (-epsilon) * c
         bo_co = a / ((1 - theta) * q_t1)
@@ -208,24 +213,33 @@ def F_xx(state , c_vec):
     for i in range(n_S):
         Res[i,0] = F_xx_scal(state[i,0],state[i,1],c_vec[i,0])
     return Res 
-    
-def main_loop(c_guess,coeff,coeff_e,outer_loop = None,n_a1 = n_a):
+'Inverting aprime_func at a_lower for each state variable'
+def c_bounds(s,c_vec,aprime1 = None):
+    n_S = len(s)
+    if aprime1 == None:
+        aprime1 = a_lower * np.ones((n_S,1))
+    elif aprime1 == 'upper' :
+        aprime1 = a_upper * np.ones((n_S,1))
+    def c_solve(c):
+        return (aprime1 - aprimefunc(s,c))[:,0]
+    sol = root(c_solve,c_vec[:,0], method='hybr')   
+    return np.reshape(sol.x,(n_S,1))
+def main_loop(coeff,coeff_e,c_vec = c_guess,outer_loop = None,n_a1 = n_a,dampen_coeff = dampen_coeff_start):
     'Quantities'
     conv1 = 2.0
     iteration = 0
-    c_vec = c_guess
-    n_a1 = n_a
-    #n_S = n_a * n_z 
     agrid = np.linspace(a_lower,a_upper, n_a1)
     agrid = np.reshape(agrid,(n_a1,1))
     s =  np.concatenate((np.kron(np.ones((n_z,1)),agrid),np.kron(zgrid,np.ones((n_a1,1)))),1)
+    c_vec = np.minimum(c_vec,c_bounds(s,c_vec))
+    c_vec = np.maximum(c_vec,c_bounds(s,c_vec , 'upper'))
+    aprime = aprimefunc(s,c_vec)
     while conv1 > 1e-3:
         conv2 = 10.0
-        iteration = iteration + 1                
+        iteration = iteration + 1
+        if iteration > fast_coeff:
+            dampen_coeff = 1.0
         while conv2 > 1e-3:
-            aprime = aprimefunc(s,c_vec)
-            #aprime = np.minimum(aprimefunc(s,c_vec),a_upper * np.ones((n_s,1)))        
-            #aprime = np.maximum(aprime,a_lower * np.ones((n_s,1)))
             aprime_c = aprimefunc_x(s,c_vec)
             aprime_cc = aprimefunc_xx(s,c_vec)        
             sprime = np.concatenate((aprime,s[:,1,None]),axis = 1)
@@ -236,84 +250,35 @@ def main_loop(c_guess,coeff,coeff_e,outer_loop = None,n_a1 = n_a):
             Hessian = F_xx(s,c_vec) + beta *( np.multiply(Phi_xps1@coeff_e,aprime_cc ) + np.multiply(Phi_xps2@coeff_e, (aprime_c)**2 ))
             Jacobian = F_x(s,c_vec) + beta *( np.multiply(Phi_xps1@coeff_e,aprime_c )) 
             
-            c_vec_next = newton_method(c_vec,Jacobian,Hessian,dampen_newton)
-            
+            c_vec_next = np.minimum(newton_method(c_vec,Jacobian,Hessian,dampen_newton),c_bounds(s,c_vec))
+            c_vec_next = np.maximum(c_vec_next,c_bounds(s,c_vec , 'upper'))
+            #c_vec_next = newton_method(c_vec,Jacobian,Hessian,dampen_newton)
             conv2 = np.max( np.absolute (c_vec_next - c_vec ))
             c_vec = c_vec_next
-            
-        conv1, coeff , coeff_e = newton_iter(coeff,coeff_e,dampen_coeff,s,c_vec,sprime,Phi_s,F)
-        print((conv1,conv2))
+            aprime = aprimefunc(s,c_vec)
+            print(conv2)
         if outer_loop == 1:
             'Computing the stationary distribution'
             conv1 = 0
             Q_x = ip.spli_basex((n_a1,a_lower,a_upper),aprime[:,0],knots = None , deg= 1,order = 0)
             Q_z = np.kron(T,np.ones((n_a1,1)))
-            Q = ip.dprod(Q_x,Q_z)
+            Q = ip.dprod(Q_z,Q_x)
             w , v = slin.eig(Q.transpose())
             L = (v[:,0] / v[:,0].real.sum(0)).real
             agra = np.dot(L,aprime) # Aggregate asset level
-            Res = (L,agra)
+            Res = (L,agra,c_vec)
         else:
-            Res = (coeff, coeff_e)
+            conv1, coeff , coeff_e = newton_iter(coeff,coeff_e,dampen_coeff,s,c_vec,sprime,Phi_s,F)
+            Res = (coeff, coeff_e,c_vec)
+        print((conv1,conv2))
     return Res
 'Prices (fixed for now)'
-q_t=1.0
-q_t1=1.0
+q_t = 1.1
+q_t1 = 1.1
 w = 1.0
-r = 0.016
+r = 0.01
 u = (1. + r) * q_t1 / q_t - 1. 
-coeff, coeff_e = main_loop(c_guess,coeff_guess,coeff_e_guess)
-L , agra = main_loop(c_guess,coeff,coeff_e,1,50)    
+coeff, coeff_e ,c_vec1 = main_loop(coeff_guess,coeff_e_guess)
+c_guess1 = 1.0 * np.ones((n_ds,1))
+L , agra, c_vec2 = main_loop(coeff,coeff_e,c_guess1,1,n_d)    
 plt.plot(L)
-#
-#n_d = 10 # number of grid points for the asset distribution in finer grid
-#n_ds = n_d * n_z 
-#a_fine_grid = np.linspace(a_lower,a_upper, n_d)
-#a_fine_grid = np.reshape(a_fine_grid,(n_d,1))
-#s_fine_grid = np.concatenate((np.kron(np.ones((n_z,1)),a_fine_grid),np.kron(zgrid,np.ones((n_d,1)))),1)
-#
-##cc = np.reshape(np.linspace(0.1, 5., n_d),(n_d,1))
-##c_vec_fine_grid = np.vstack((cc,cc,cc,cc,cc))
-#c_vec_fine_grid = c_guess#1.0 * np.ones((n_ds,1)) 
-#conv3 = 1
-#while conv3 > 0.01:
-#    aprime_fine_grid = aprimefunc(s_fine_grid,c_vec_fine_grid)
-#    #aprime_fine_grid = np.minimum(aprimefunc(s_fine_grid,c_vec_fine_grid),a_upper * np.ones((n_ds,1)))        
-#    #aprime_fine_grid = np.maximum(aprime_fine_grid,a_lower * np.ones((n_ds,1)))
-#    aprime_c_fine_grid = aprimefunc_x(s_fine_grid,c_vec_fine_grid)
-#    aprime_cc_fine_grid = aprimefunc_xx(s_fine_grid,c_vec_fine_grid)        
-#    sprime_fine_grid = np.concatenate((aprime_fine_grid,s_fine_grid[:,1,None]),axis = 1)
-#    Phi_xps3 = ip.funbas(P,sprime_fine_grid,(1,0),Polyname)
-#    Phi_xps4 = ip.funbas(P,sprime_fine_grid,(2,0),Polyname)
-#    
-#    
-#    Hessian_fine_grid = F_xx(s_fine_grid,c_vec_fine_grid) + beta *( np.multiply(Phi_xps3@coeff_e,aprime_cc_fine_grid ) + np.multiply(Phi_xps4@coeff_e, (aprime_c_fine_grid)**2 ))
-#    Jacobian_fine_grid = F_x(s_fine_grid,c_vec_fine_grid) + beta *( np.multiply(Phi_xps4@coeff_e,aprime_c_fine_grid )) 
-#    
-#    c_vec_next_fine_grid = newton_method(c_vec_fine_grid,Jacobian_fine_grid,Hessian_fine_grid,dampen_newton)
-#    #c_vec_next = np.maximum(c_vec_next, np.zeros((n_s,1)))
-#    #c_vec_next = np.minimum(c_vec_next, a_upper * np.ones((n_s,1)))
-#    
-#    conv3 = np.max( np.absolute (c_vec_next_fine_grid - c_vec_fine_grid ))
-#    c_vec_fine_grid = c_vec_next_fine_grid
-#    print((conv3))
-#       
-#    
-    
-#    sprime_fine_grid = np.concatenate((aprime_fine_grid,s_fine_grid[:,1,None]),axis = 1)
-#    Phi_xps1_fine_grid = ip.funbas(P,sprime_fine_grid,(1,0),Polyname)
-#    Phi_xps2_fine_grid = ip.funbas(P,sprime_fine_grid,(2,0),Polyname)
-#    aprime_next_fine_grid = aprime_fine_grid -dampen_newton * np.multiply( 1 / (F_xx(s_fine_grid,aprime_fine_grid) + beta * Phi_xps2_fine_grid @ coeff) ,(F_x(s_fine_grid,aprime_fine_grid) + beta * Phi_xps1_fine_grid @ coeff_e))
-#    aprime_next_fine_grid = np.maximum(aprime_next_fine_grid, a_lower * np.ones((n_ds,1)))
-#    aprime_next_fine_grid = np.minimum(aprime_next_fine_grid, a_upper * np.ones((n_ds,1)))
-#    conv3 = np.max( np.absolute (aprime_next_fine_grid - aprime_fine_grid ))
-#    print(conv3)
-#    aprime_fine_grid = aprime_next_fine_grid
-#    
-#Q_x = ip.spli_basex((n_d,a_lower,a_upper),aprime_fine_grid[:,0],knots = None , deg= 1,order = 0)
-#Q_z = np.kron(T,np.ones((n_d,1)))
-#Q = ip.dprod(Q_x,Q_z)
-#w , v = slin.eig(Q.transpose())
-#L = (v[:,0] / v[:,0].real.sum(0)).real
-#plt.plot(L)
-#print(np.dot(L,aprime_fine_grid))
